705887abbe992376f8cd30433f353203ee2cdbb2
Zigi112 <azizjd543@gmail.com>
2024-12-06 18:02:52 +0100

    deuxieme push

--- src/main/java/com/example/repositry/EventRepository.java
+++ src/main/java/com/example/repositry/EventRepository.java
@@ -1,8 +1,6 @@
 package com.example.repositry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Model.Contact;
 import com.example.Model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
--- src/main/java/com/example/repositry/contactRepository.java
+++ src/main/java/com/example/repositry/contactRepository.java
@@ -1,9 +1,7 @@
 package com.example.repositry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.Model.Contact;
-import com.example.Model.Personne;
 
 public interface contactRepository extends JpaRepository<Contact, Long> {
 
